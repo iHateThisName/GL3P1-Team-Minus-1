7cdff3943949e6a0dfa0f378a292aac6d26148b1
Myre36 <halldorb04@gmail.com>
2025-11-17 14:37:02 +0100

    Added more checkpoint

--- Assets/_Scripts/CheckPointManager.cs
+++ Assets/_Scripts/CheckPointManager.cs
@@ -54,6 +54,6 @@ public class CheckPointManager : Singleton<CheckPointManager> {
     }
 
     public enum EnumCheckPoint : int {
-        None = -1, Store = 0, DawnCheckPoint = 1, TwilightCheckPoint = 2,
+        None = -1, Store = 0, DawnCheckPoint = 1, TwilightCheckPoint = 2, LeftMidnightCheckPoint = 3, MiddleMidnightCheckPoint = 4,
     }
 }
