d61e1c1050c747530227d850bb9a966bf9442c16
ariana <ariana.roxana@gmail.com>
2025-11-27 15:18:34 +0100

    shader script fix and global volumes

--- Assets/_Scripts/Ari NUCLEAR ZONE/UnderwaterDepth.cs
+++ Assets/_Scripts/Ari NUCLEAR ZONE/UnderwaterDepth.cs
@@ -1,47 +1,110 @@
-using System;
 using UnityEngine;
 using UnityEngine.Rendering;
-
 public class UnderwaterDepth : MonoBehaviour
 {
     [Header("Depth Parameters")]
     [SerializeField] private Transform mainCamera;
-    [SerializeField] private int depth = 0;
+    [SerializeField] private int UpperWater = 0;
+    [SerializeField] private int MiddleWater = -100;
+    [SerializeField] private int VoidWater = -200;
 
     [Header("Post Processing Volume")]
     [SerializeField] private Volume postProcessingVolume;
 
     [Header("Post Processing Profiles")]
     [SerializeField] private VolumeProfile surfacePostProcessing;
-    [SerializeField] private VolumeProfile underwaterPostProcessing;
+    [SerializeField] private VolumeProfile UpperWaterPostProcessing;
+    [SerializeField] private VolumeProfile MiddleWaterPostProcessing;
+    [SerializeField] private VolumeProfile VoidWaterPostProcessing;
 
-    private void Update()
+    private EnumVolumProfile currentVolumeProfile;
+
+    private void Start()
     {
-        if (mainCamera.position.y < depth)
+        currentVolumeProfile = EnumVolumProfile.Surface;
+        RenderSettings.fog = false;
+        postProcessingVolume.profile = surfacePostProcessing;
+    }
+
+    private void LateUpdate()
+    {
+        UpdateWaterVolume();
+    }
+
+    private void UpdateWaterVolume()
+    {
+        if (mainCamera.position.y < UpperWater)
         {
-            EnableEffects(true);
+            if (mainCamera.position.y < MiddleWater)
+            {
+
+                if (mainCamera.position.y < VoidWater)
+                {
+                    EnableEffects(EnumVolumProfile.VoidWater);
+                }
+                else
+                {
+                    EnableEffects(EnumVolumProfile.MiddleWater);
+                }
+
+            }
+            else
+            {
+                EnableEffects(EnumVolumProfile.UpperWater);
+            }
+
         }
         else
         {
-            EnableEffects(false);
+            EnableEffects(EnumVolumProfile.Surface);
         }
     }
 
-    private void EnableEffects(bool active)
+    private void EnableEffects(EnumVolumProfile newProfile)
     {
-        if(active)
+
+        if (newProfile == currentVolumeProfile) return;
+
+        currentVolumeProfile = newProfile;
+
+        if (newProfile == EnumVolumProfile.Surface)
         {
-            postProcessingVolume.profile = underwaterPostProcessing;
+            RenderSettings.fog = false;
         }
         else
         {
-            postProcessingVolume.profile= surfacePostProcessing;
+            RenderSettings.fog = true;
         }
+
+        switch (newProfile)
+        {
+            case EnumVolumProfile.Surface:
+                postProcessingVolume.profile = surfacePostProcessing;
+                return;
+
+            case EnumVolumProfile.UpperWater:
+                postProcessingVolume.profile = UpperWaterPostProcessing;
+                return;
+
+            case EnumVolumProfile.MiddleWater:
+                postProcessingVolume.profile = MiddleWaterPostProcessing;
+                return;
+
+            case EnumVolumProfile.VoidWater:
+                postProcessingVolume.profile = VoidWaterPostProcessing;
+                return;
+        }
+
+    }
+
+    private enum EnumVolumProfile
+    {
+        Surface, UpperWater, MiddleWater, VoidWater
     }
 }
 
-    
 
-    
 
-    
+
+
+
