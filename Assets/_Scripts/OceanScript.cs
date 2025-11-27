f1b85d16a0f923576a2d3fc5650b8f8eaf369cce
Myre36 <67600948+Myre36@users.noreply.github.com>
2025-11-27 11:08:23 +0100

    Added the tutorial function

--- Assets/_Scripts/OceanScript.cs
+++ Assets/_Scripts/OceanScript.cs
@@ -4,6 +4,11 @@ public class OceanScript : MonoBehaviour {
 
     void OnTriggerEnter(Collider other) {
         if (other.CompareTag("PlayerHead")) {
+            if(!GameManager.Instance.firstEnteredOcean)
+            {
+                //GameSceneManager.Instance.
+            }
+
             GameManager.Instance.PlayerEnterOcean();
         }
     }
