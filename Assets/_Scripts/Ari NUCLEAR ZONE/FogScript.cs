9cc962a93570d8c864d8995aabde6ac3a5218762
Myre36 <67600948+Myre36@users.noreply.github.com>
2025-11-28 20:44:49 +0100

    Fixing fog settings

--- Assets/_Scripts/Ari NUCLEAR ZONE/FogScript.cs
+++ Assets/_Scripts/Ari NUCLEAR ZONE/FogScript.cs
@@ -5,7 +5,15 @@ public class FogScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y > 0) RenderSettings.fog = false;
-        else if (transform.position.y < 0) RenderSettings.fog = true;
+        if (transform.position.y > 0)
+        { 
+            RenderSettings.fog = false;
+            Debug.Log("Turning off fog");
+        }
+        else if (transform.position.y < 0)
+        {
+            RenderSettings.fog = true;
+            Debug.Log("Turning on fog");
+        }
     }
 }
