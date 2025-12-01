63c6156fc78b182c6c22fccf21116c20ab2ca5e4
iHateThisName <ivarravi0@gmail.com>
2025-12-01 14:05:59 +0100

    Feature Added, Shark Bite Animation

--- Assets/_Scripts/AI Scripts/SharkDamagePlayer.cs
+++ Assets/_Scripts/AI Scripts/SharkDamagePlayer.cs
@@ -1,17 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
-public class SharkDamagePlayer : MonoBehaviour
-{
-    [SerializeField]
-    private int damage;
+public class SharkDamagePlayer : MonoBehaviour {
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            GameManager.Instance.BreathingScript.TakeDamage(damage);
+    [SerializeField] private int damage;
+    [SerializeField] private Animator animationAnimator;
 
-            GameManager.Instance.PlayerMovement.GetRigidbody().AddForce(transform.forward * 25f, ForceMode.Impulse);
+    private const string BiteTrigger = "BiteTrigger";
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            StartCoroutine(BiteAnimationCoroutine());
         }
+    }
+
+    private IEnumerator BiteAnimationCoroutine() {
+        animationAnimator.SetTrigger(BiteTrigger);
+        yield return new WaitForSeconds(0.25f);
+        GameManager.Instance.BreathingScript.TakeDamage(damage);
+        GameManager.Instance.PlayerMovement.GetRigidbody().AddForce(transform.forward * 25f, ForceMode.Impulse);
     }
 }
